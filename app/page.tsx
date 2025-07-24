18bf24d8b5e315e85ae085beb9424c772a46f485
Ingo Wolf <me@ingowolf.au>
2025-07-24 19:19:41 +1000

    add some ui elements

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,30 @@
 import Image from "next/image";
 import Link from "next/link";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen">
+    <div className="h-screen w-screen flex flex-col">
       <div className="flex flex-row container m-auto p-3 gap-3">
         <Link href="/">xkcd-webapp</Link>
         <div className="grow"></div>
         <Link href="/saved">Saved</Link>
+      </div>
+      <div className="flex w-full h-full justify-center items-center">
+        <div className="flex flex-col gap-3 max-w-[95%] max-h-[95%]">
+          <Card className="p-3">
+            <img src="https://imgs.xkcd.com/comics/flettner_rotor.png" alt="" />
+          </Card>
+        </div>
       </div>
     </div>
   );
