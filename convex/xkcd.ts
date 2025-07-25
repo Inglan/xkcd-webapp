91fbe72c6002a925a538e188d1a02c8beba46287
Ingo Wolf <me@ingowolf.au>
2025-07-25 17:27:00 +1000

    add xkcd api

---
+++ convex/xkcd.ts
@@ -0,0 +1,25 @@
+import { v } from "convex/values";
+import { action } from "./_generated/server";
+
+export const getById = action({
+  handler: async (ctx, args) => {
+    const data = await fetch(`https://xkcd.com/info.${args.id}.json`);
+    const json = (await data.json()) as {
+      month: string;
+      num: number;
+      link: string;
+      year: string;
+      news: string;
+      safe_title: string;
+      transcript: string;
+      alt: string;
+      img: string;
+      title: string;
+      day: string;
+    };
+    return json;
+  },
+  args: {
+    id: v.number(),
+  },
+});
