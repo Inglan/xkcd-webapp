5420ff82dae6b4fba233832b7f6b5ca886cfb2d4
Ingo Wolf <me@ingowolf.au>
2025-07-25 16:17:19 +1000

    resdesign

--- app/page.tsx
+++ app/page.tsx
@@ -17,56 +17,62 @@ import {
   Download,
   EllipsisVertical,
   Share,
+  Text,
 } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen flex flex-col">
+    <div className="h-screen w-screen flex flex-col items-center">
       <div className="flex flex-row container m-auto p-3 gap-3">
         <Link href="/">xkcd-webapp</Link>
         <div className="grow"></div>
         <Link href="/saved">Saved</Link>
       </div>
-      <div className="flex w-full h-full justify-center items-center">
+
+      <div className="flex w-full h-full justify-center items-center flex-col">
         <div className="flex flex-col gap-3 max-w-[95%] max-h-[95%]">
           <img src="https://imgs.xkcd.com/comics/flettner_rotor.png" alt="" />
-          <div className="flex gap-2 w-full">
-            <Button size="icon" variant="ghost">
-              <ChevronLeft />
-            </Button>
-            <Button size="icon" variant="ghost">
-              <ChevronRight />
-            </Button>
-            <Button size="icon" variant="ghost">
-              <Dice5 />
-            </Button>
-            <div className="grow"></div>
-            <Button size="icon" variant="ghost">
-              <Bookmark />
-            </Button>
-            <DropdownMenu>
-              <DropdownMenuTrigger
-                className={buttonVariants({ size: "icon", variant: "ghost" })}
-              >
-                <EllipsisVertical />
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuItem>
-                  <Share />
-                  Share
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <Download />
-                  Download
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <Book />
-                  Explain
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </div>
         </div>
+      </div>
+
+      <div className="flex gap-2 p-3 w-full md:w-fit">
+        <Button size="icon" variant="ghost">
+          <ChevronLeft />
+        </Button>
+        <Button size="icon" variant="ghost">
+          <ChevronRight />
+        </Button>
+        <Button size="icon" variant="ghost">
+          <Dice5 />
+        </Button>
+        <div className="grow"></div>
+        <Button size="icon" variant="ghost">
+          <Bookmark />
+        </Button>
+        <Button size="icon" variant="ghost">
+          <Text />
+        </Button>
+        <DropdownMenu>
+          <DropdownMenuTrigger
+            className={buttonVariants({ size: "icon", variant: "ghost" })}
+          >
+            <EllipsisVertical />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem>
+              <Share />
+              Share
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Download />
+              Download
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Book />
+              Explain
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </div>
   );
