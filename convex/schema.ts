5d22762a4a1170cddf377a2c17d786a2caf284ca
Ingo Wolf <me@ingowolf.au>
2025-07-25 17:07:05 +1000

    add favourites to schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,7 +1,12 @@
-import { defineSchema } from "convex/server";
+import { defineSchema, defineTable } from "convex/server";
 import { authTables } from "@convex-dev/auth/server";
+import { v } from "convex/values";
 
 const schema = defineSchema({
+  favourites: defineTable({
+    num: v.number(),
+    user: v.id("users"),
+  }),
   ...authTables,
 });
 
