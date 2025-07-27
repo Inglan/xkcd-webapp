e186411c05cf8ad4ff54f0e624cf38ef626a8061
Ingo Wolf <me@ingowolf.au>
2025-07-27 13:48:41 +1000

    make button ghost

--- app/page.tsx
+++ app/page.tsx
@@ -106,7 +106,7 @@ export default function Home() {
           </DrawerContent>
         </Drawer>
         <Button
-          variant="outline"
+          variant="ghost"
           size="icon"
           onClick={() => {
             setTheme(theme == "dark" ? "light" : "dark");
