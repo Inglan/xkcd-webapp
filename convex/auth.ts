07eb8893f409aba784ac51a688a88baf2d496e24
Ingo Wolf <me@ingowolf.au>
2025-07-27 16:40:22 +1000

    add anonymous auth

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,6 +1,7 @@
 import { convexAuth } from "@convex-dev/auth/server";
 import GitHub from "@auth/core/providers/github";
+import { Anonymous } from "@convex-dev/auth/providers/Anonymous";
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
-  providers: [GitHub],
+  providers: [GitHub, Anonymous],
 });
