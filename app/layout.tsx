3cad0884812f9236aa22deb79469118f974df568
Ingo Wolf <me@ingowolf.au>
2025-07-27 13:50:13 +1000

    default to light theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
           <ConvexClientProvider>
-            <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
               {children}
               <Toaster />
             </ThemeProvider>
