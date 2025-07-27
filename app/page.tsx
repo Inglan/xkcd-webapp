df3ef167c6371f928cb7235ec3f9a949151066d2
Ingo Wolf <me@ingowolf.au>
2025-07-27 16:32:19 +1000

    overflow saved comics

--- app/page.tsx
+++ app/page.tsx
@@ -149,7 +149,7 @@ export default function Home() {
               <DrawerHeader>
                 <DrawerTitle>Saved</DrawerTitle>
               </DrawerHeader>
-              <div className="p-4 flex flex-col gap-3">
+              <div className="p-4 flex flex-col gap-3 overflow-y-auto">
                 <AnimatePresence>
                   {saves &&
                     saves.map((save) => (
