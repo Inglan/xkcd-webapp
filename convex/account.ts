ea741959a4fcab6f2c361a290b2ae2510b534e70
Ingo Wolf <me@ingowolf.au>
2025-07-27 17:09:11 +1000

    add delete user function

--- convex/account.ts
+++ convex/account.ts
@@ -29,3 +29,49 @@ export const info = query({
     }
   },
 });
+
+export const deleteAccount = mutation({
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) {
+      throw new Error("Not authenticated");
+    }
+
+    await ctx.db.delete(userId);
+
+    const authAccountsEntries = await ctx.db
+      .query("authAccounts")
+      .withIndex("userIdAndProvider", (q) => q.eq("userId", userId))
+      .collect();
+
+    for (const entry of authAccountsEntries) {
+      await ctx.db.delete(entry._id);
+    }
+
+    const authSessionsEntries = await ctx.db
+      .query("authSessions")
+      .withIndex("userId", (q) => q.eq("userId", userId))
+      .collect();
+
+    for (const entry of authSessionsEntries) {
+      const authRefreshTokens = await ctx.db
+        .query("authRefreshTokens")
+        .withIndex("sessionId", (q) => q.eq("sessionId", entry._id))
+        .collect();
+      for (const token of authRefreshTokens) {
+        await ctx.db.delete(token._id);
+      }
+      await ctx.db.delete(entry._id);
+    }
+
+    const savesEntries = await ctx.db
+      .query("saves")
+      .withIndex("by_user", (q) => q.eq("user", userId))
+      .collect();
+
+    for (const entry of savesEntries) {
+      await ctx.db.delete(entry._id);
+    }
+  },
+});
