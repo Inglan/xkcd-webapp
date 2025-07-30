08df08ab6fe86902fbc34df45e51bec619d50b99
Ingo Wolf <me@ingowolf.au>
2025-07-30 18:48:52 +1000

    change caching logic a lot

--- convex/xkcd.ts
+++ convex/xkcd.ts
@@ -1,13 +1,54 @@
 import { v } from "convex/values";
 import {
   action,
+  ActionCtx,
   internalAction,
   internalMutation,
   internalQuery,
   query,
 } from "./_generated/server";
 import { internal } from "./_generated/api";
-import { Id } from "./_generated/dataModel";
+import { Doc, Id } from "./_generated/dataModel";
+import { cache } from "react";
+
+async function cacheComic(
+  data: {
+    month: string;
+    num: number;
+    link: string;
+    year: string;
+    news: string;
+    safe_title: string;
+    transcript: string;
+    alt: string;
+    img: string;
+    title: string;
+    day: string;
+  },
+  ctx: ActionCtx,
+) {
+  const imageUrl = data.img;
+
+  const response = await fetch(imageUrl);
+  const image = await response.blob();
+
+  const storageId: Id<"_storage"> = await ctx.storage.store(image);
+
+  await ctx.runMutation(internal.xkcd.writeCachedComic, {
+    month: data.month,
+    num: data.num,
+    link: data.link,
+    year: data.year,
+    news: data.news,
+    safe_title: data.safe_title,
+    transcript: data.transcript,
+    alt: data.alt,
+    img: data.img,
+    title: data.title,
+    day: data.day,
+    storageId: storageId,
+  });
+}
 
 export const getCachedById = internalQuery({
   handler: async (ctx, args) => {
@@ -18,45 +59,6 @@ export const getCachedById = internalQuery({
   },
   args: {
     id: v.number(),
-  },
-});
-
-export const cacheComic = internalAction({
-  handler: async (ctx, args) => {
-    const imageUrl = args.img;
-
-    const response = await fetch(imageUrl);
-    const image = await response.blob();
-
-    const storageId: Id<"_storage"> = await ctx.storage.store(image);
-
-    await ctx.runMutation(internal.xkcd.writeCachedComic, {
-      month: args.month,
-      num: args.num,
-      link: args.link,
-      year: args.year,
-      news: args.news,
-      safe_title: args.safe_title,
-      transcript: args.transcript,
-      alt: args.alt,
-      img: args.img,
-      title: args.title,
-      day: args.day,
-      storageId: storageId,
-    });
-  },
-  args: {
-    month: v.string(),
-    num: v.number(),
-    link: v.string(),
-    year: v.string(),
-    news: v.string(),
-    safe_title: v.string(),
-    transcript: v.string(),
-    alt: v.string(),
-    img: v.string(),
-    title: v.string(),
-    day: v.string(),
   },
 });
 
@@ -142,19 +144,7 @@ export const getById = action({
       day: string;
     };
 
-    await ctx.runAction(internal.xkcd.cacheComic, {
-      month: json.month,
-      num: json.num,
-      link: json.link,
-      year: json.year,
-      news: json.news,
-      safe_title: json.safe_title,
-      transcript: json.transcript,
-      alt: json.alt,
-      img: json.img,
-      title: json.title,
-      day: json.day,
-    });
+    await cacheComic(json, ctx);
 
     return json;
   },
@@ -197,19 +187,7 @@ export const getLatest = action({
     });
 
     if (cachedComic.length == 0) {
-      await ctx.runAction(internal.xkcd.cacheComic, {
-        month: json.month,
-        num: json.num,
-        link: json.link,
-        year: json.year,
-        news: json.news,
-        safe_title: json.safe_title,
-        transcript: json.transcript,
-        alt: json.alt,
-        img: json.img,
-        title: json.title,
-        day: json.day,
-      });
+      await cacheComic(json, ctx);
     }
 
     return json;
